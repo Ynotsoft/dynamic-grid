bea9b86cda1075ba940c0e2a7a12af4528a7f5f0
Omar <Omar.ghonim@icloud.com>
2025-10-22 14:56:19 +1100

    fix: update pageSizes array to remove unsupported values

--- src/DynamicGrid/hooks/useGridData.jsx
+++ src/DynamicGrid/hooks/useGridData.jsx
@@ -150,7 +150,7 @@ export default function useGridData({
     setReverse((r) => !r);
   };
 
-  const pageSizes = useMemo(() => [20, 50, 100, 200, 500], []);
+  const pageSizes = useMemo(() => [20, 50, 75, 100], []);
 
   return {
     totalCount,
